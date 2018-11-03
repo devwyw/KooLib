bc750e8f8f48ac72a7b8a8dfe55e6b303d6a191e
weiyanwu <weiyanwu@koolearn-inc.com>
2018-11-03 15:21:50 +0800

    add

---
+++ KooLib/Classes/KooLib.h
@@ -0,0 +1,18 @@
+//
+//  KooLib.h
+//  KooLib
+//
+//  Created by weiyanwu on 2018/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KooLib : NSObject
+
++(void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KooLib/Classes/KooLib.m
@@ -0,0 +1,17 @@
+//
+//  KooLib.m
+//  KooLib
+//
+//  Created by weiyanwu on 2018/11/3.
+//
+
+#import "KooLib.h"
+
+@implementation KooLib
+
++(void)log
+{
+    NSLog(@"HELLO world!");
+}
+
+@end
